0d945b7e158e171cb52adec71caa059878f77f26
SnehaAPatil <9patilsneha@gmail.com>
2020-01-18 14:36:50 +0530

    angular MPT

---
+++ Angular-MPT/project 1/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CustomerComponent } from './customer/customer.component';
+import { ProductComponent } from './product/product.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { DataFromJsonComponent } from './data-from-json/data-from-json.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CustomerComponent,
+    ProductComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageNotFoundComponent,
+    DataFromJsonComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular-MPT/project 2/src/app/get-all-posts/get-all-posts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-get-all-posts',
+  templateUrl: './get-all-posts.component.html',
+  styleUrls: ['./get-all-posts.component.css']
+})
+export class GetAllPostsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
